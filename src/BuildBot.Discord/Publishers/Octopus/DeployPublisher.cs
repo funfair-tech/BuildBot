51f7e4ea3b2268bacbb1b9b6dcbe7cfc50b0e2db
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-04-07 00:08:49 +0100

    Added initial endpoint for octopus

---
+++ src/BuildBot.Discord/Publishers/Octopus/DeployPublisher.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using BuildBot.ServiceModel.Octopus;
+using Discord;
+
+namespace BuildBot.Discord.Publishers.Octopus
+{
+    public class DeployPublisher : IPublisher<Deploy>
+    {
+        private readonly IDiscordBot _bot;
+
+        public DeployPublisher(IDiscordBot bot)
+        {
+            this._bot = bot;
+        }
+
+        public Task PublishAsync(Deploy message)
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.WithTitle(message.Payload.Event.Message);
+            builder.WithUrl(message.Payload.ServerAuditUri);
+
+            if (message.Payload.Event.Category == "DeploymentSucceeded")
+            {
+                builder.Color = Color.Green;
+            }
+
+            if (message.Payload.Event.Category == "DeploymentFailed")
+            {
+                builder.Color = Color.Red;
+            }
+
+            return this._bot.PublishAsync(builder);
+        }
+    }
+}
