7abdf32f75e9a622da052dce4990cd3f47eb483c
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-29 13:59:29 +0100

    Switched to use init

--- src/BuildBot.Discord/DiscordBotConfiguration.cs
+++ src/BuildBot.Discord/DiscordBotConfiguration.cs
@@ -5,17 +5,17 @@ namespace BuildBot.Discord
 {
     public sealed class DiscordBotConfiguration
     {
-        public string Token { get; set; } = default!;
+        public string Token { get; init; } = default!;
 
-        public string Server { get; set; } = default!;
+        public string Server { get; init; } = default!;
 
-        public string Channel { get; set; } = default!;
+        public string Channel { get; init; } = default!;
 
-        public string ReleaseChannel { get; set; } = default!;
+        public string ReleaseChannel { get; init; } = default!;
 
         public static DiscordBotConfiguration Load(string jsonFile)
         {
-            return JsonConvert.DeserializeObject<DiscordBotConfiguration>(File.ReadAllText(jsonFile));
+            return JsonConvert.DeserializeObject<DiscordBotConfiguration>(File.ReadAllText(jsonFile))!;
         }
     }
 }
