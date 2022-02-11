acb1b007334e4c1e66ef74400b7441743a7c2745
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-02-11 15:30:59 +0000

    used Json property name

--- src/BuildBot.ServiceModel/Octopus/Deploy.cs
+++ src/BuildBot.ServiceModel/Octopus/Deploy.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace BuildBot.ServiceModel.Octopus;
 
 //https://octopus.com/blog/notifications-with-subscriptions-and-webhooks
 public sealed class Deploy
 {
+    [JsonPropertyName("Timestamp")]
     public DateTime Timestamp { get; init; }
 
+    [JsonPropertyName("EventType")]
     public string EventType { get; init; } = default!;
 
     [SuppressMessage(category: "ReSharper", checkId: "UnusedAutoPropertyAccessor.Global", Justification = "TODO: Review")]
+    [JsonPropertyName("Payload")]
     public DeployPayload? Payload { get; init; }
 }
