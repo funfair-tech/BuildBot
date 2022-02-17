256b6347d1099714c76c646b6650d6c0106bd762
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-02-17 21:11:11 +0000

    Serialisation + metadata

--- src/BuildBot.Json/SerializationContext.cs
+++ src/BuildBot.Json/SerializationContext.cs
@@ -3,6 +3,7 @@ using BuildBot.ServiceModel.Octopus;
 
 namespace BuildBot.Json;
 
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization | JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(Deploy))]
 [JsonSerializable(typeof(DeployPayload))]
 [JsonSerializable(typeof(DeploymentPayloadSubscription))]
