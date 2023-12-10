179fcbd392453fe15555f4b998dd0b77b7d12809
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-10 15:10:54 +0000

    Fixed code analysis issues

--- src/BuildBot/Helpers/VersionDetection.cs
+++ src/BuildBot/Helpers/VersionDetection.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 
 namespace BuildBot.Helpers;
 
@@ -13,61 +6,5 @@ namespace BuildBot.Helpers;
 internal static class VersionDetection
 {
     [SuppressMessage(category: "ReSharper", checkId: "UnusedMember.Global", Justification = "Used in exe code. Not possible to unit test.")]
-    public static string ProgramVersion(Type programType)
-    {
-        Assembly assembly = programType.Assembly;
-
-        return GetAssemblyFileVersionFile() ?? GetAssemblyFileVersion(assembly) ?? GetAssemblyVersion(assembly);
-    }
-
-    private static string? GetAssemblyFileVersionFile()
-    {
-        IReadOnlyList<string> args = Environment.GetCommandLineArgs();
-
-        if (args.Count == 0)
-        {
-            return null;
-        }
-
-        string location = args[0];
-
-        if (string.IsNullOrWhiteSpace(location) || !File.Exists(location))
-        {
-            return null;
-        }
-
-        FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(location);
-
-        return fileVersionInfo.ProductVersion!;
-    }
-
-    private static string? GetAssemblyFileVersion(Assembly assembly)
-    {
-        AssemblyFileVersionAttribute? fvi = assembly.GetCustomAttributes<AssemblyFileVersionAttribute>()
-                                                    .FirstOrDefault();
-
-        if (fvi is null)
-        {
-            Console.WriteLine("Finding Assembly File Version: No Attribute found");
-
-            return null;
-        }
-
-        return fvi.Version;
-    }
-
-    private static string GetAssemblyVersion(Assembly assembly)
-    {
-        Version? assemblyVersion = assembly.GetName()
-                                           .Version;
-
-        if (assemblyVersion is null)
-        {
-            Console.WriteLine("Finding Assembly Version: No Assembly Version");
-
-            throw new VersionNotFoundException();
-        }
-
-        return assemblyVersion.ToString();
-    }
+    public const string ProgramVersion = ThisAssembly.Info.FileVersion;
 }
