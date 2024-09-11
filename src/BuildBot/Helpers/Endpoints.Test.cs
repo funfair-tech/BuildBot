9196b8b8f3d5f06c625645179015466424593134
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-09-11 11:30:10 +0100

    Ping

--- src/BuildBot/Helpers/Endpoints.Test.cs
+++ src/BuildBot/Helpers/Endpoints.Test.cs
@@ -1,5 +1,8 @@
+using BuildBot.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace BuildBot.Helpers;
 
@@ -7,8 +10,26 @@ internal static partial class Endpoints
 {
     private static WebApplication ConfigureTestEndpoints(this WebApplication app)
     {
-        app.MapGet(pattern: "/ping", handler: static () => Results.Ok(PingPong.Model));
+        app.MapGet(pattern: "/ping",
+                   handler: static ([FromQuery] string? source, Logger<PongDto> logger) =>
+                            {
+                                LogPing(source: source, logger: logger);
+
+                                return Results.Ok(PingPong.Model);
+                            });
 
         return app;
+    }
+
+    private static void LogPing(string? source, Logger<PongDto> logger)
+    {
+        if (!string.IsNullOrWhiteSpace(source))
+        {
+            logger.LogError(message: "PING: Source: {Source}", source);
+        }
+        else
+        {
+            logger.LogError("PING: NO Source");
+        }
     }
 }
