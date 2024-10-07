a84ba99fcfe1bf79b38c924b557e376aaf764958
Mark Ridgwell <credfeto@users.noreply.github.com>
2024-10-07 11:04:21 +0100

    Messaging on deployment

--- src/BuildBot.CloudFormation/Publishers/CloudFormationMessageReceivedNotificationHandler.cs
+++ src/BuildBot.CloudFormation/Publishers/CloudFormationMessageReceivedNotificationHandler.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using BuildBot.CloudFormation.Configuration;
 using BuildBot.CloudFormation.Models;
+using BuildBot.Discord.Models;
+using Discord;
 using Mediator;
 using Microsoft.Extensions.Logging;
 
@@ -33,11 +36,13 @@ public sealed class CloudFormationMessageReceivedNotificationHandler : INotifica
                                                                 };
 
     private readonly ILogger<CloudFormationMessageReceivedNotificationHandler> _logger;
+    private readonly IMediator _mediator;
     private readonly SnsNotificationOptions _options;
 
-    public CloudFormationMessageReceivedNotificationHandler(SnsNotificationOptions options, ILogger<CloudFormationMessageReceivedNotificationHandler> logger)
+    public CloudFormationMessageReceivedNotificationHandler(SnsNotificationOptions options, IMediator mediator, ILogger<CloudFormationMessageReceivedNotificationHandler> logger)
     {
         this._options = options;
+        this._mediator = mediator;
         this._logger = logger;
     }
 
@@ -48,10 +53,21 @@ public sealed class CloudFormationMessageReceivedNotificationHandler : INotifica
             return ValueTask.CompletedTask;
         }
 
-        this._logger.LogWarning(message: "CLOUDFORMATION: Received message from {TopicArn} with {MessageId} at {Timestamp}",
-                                notification.TopicArn,
-                                notification.MessageId,
-                                notification.Timestamp);
+        Deployment? deployment = this.ExtractDeploymentProperties(notification);
+
+        if (deployment is null)
+        {
+            return ValueTask.CompletedTask;
+        }
+
+        EmbedBuilder embed = BuildStatusMessage(deployment.Value);
+
+        return this._mediator.Publish(new BotMessage(embed), cancellationToken: cancellationToken);
+    }
+
+    private Deployment? ExtractDeploymentProperties(CloudFormationMessageReceived notification)
+    {
+        this._logger.LogWarning(message: "CLOUDFORMATION: Received message from {TopicArn} with {MessageId} at {Timestamp}", notification.TopicArn, notification.MessageId, notification.Timestamp);
 
         foreach ((string key, string value) in notification.Properties)
         {
@@ -60,29 +76,29 @@ public sealed class CloudFormationMessageReceivedNotificationHandler : INotifica
 
         if (!IsMatching(properties: notification.Properties, key: RESOURCE_TYPE, value: "AWS::CloudFormation::Stack"))
         {
-            return ValueTask.CompletedTask;
+            return null;
         }
 
         if (!notification.Properties.TryGetValue(key: STACK_NAME, out string? stackName))
         {
-            return ValueTask.CompletedTask;
+            return null;
         }
 
         if (!notification.Properties.TryGetValue(key: LOGICAL_RESOURCE_ID, out string? project))
         {
-            return ValueTask.CompletedTask;
+            return null;
         }
 
         this._logger.LogWarning(message: "CLOUDFORMATION: LogicalResourceId: {LogicalResourceId}", project);
 
         if (!notification.Properties.TryGetValue(key: PHYSICAL_RESOURCE_ID, out string? arn))
         {
-            return ValueTask.CompletedTask;
+            return null;
         }
 
         if (!notification.Properties.TryGetValue(key: RESOURCE_STATUS, out string? status))
         {
-            return ValueTask.CompletedTask;
+            return null;
         }
 
         this._logger.LogWarning(message: "CLOUDFORMATION: Stack Name: {StackName}", stackName);
@@ -97,7 +113,37 @@ public sealed class CloudFormationMessageReceivedNotificationHandler : INotifica
                                         : "CLOUDFORMATION: FAILED!!!");
         }
 
-        return ValueTask.CompletedTask;
+        return new Deployment(StackName: stackName, Project: project, Arn: arn, Status: status, Success: success);
+    }
+
+    private static EmbedBuilder BuildStatusMessage(in Deployment deployment)
+    {
+        return new EmbedBuilder().WithTitle($"CloudFormation {deployment.Status} for {deployment.Project} in {deployment.StackName}")
+                                 .WithColor(deployment.Success
+                                                ? Color.Green
+                                                : Color.Red)
+                                 .WithFields(GetFields(deployment));
+    }
+
+    private static IReadOnlyList<EmbedFieldBuilder> GetFields(in Deployment deployment)
+    {
+        return
+        [
+            AddArnEmbed(deployment),
+            AddStatusEmbed(deployment)
+        ];
+    }
+
+    private static EmbedFieldBuilder AddArnEmbed(in Deployment deployment)
+    {
+        return new EmbedFieldBuilder().WithName("ARN")
+                                      .WithValue(deployment.Arn);
+    }
+
+    private static EmbedFieldBuilder AddStatusEmbed(in Deployment deployment)
+    {
+        return new EmbedFieldBuilder().WithName("Status")
+                                      .WithValue(deployment.Status);
     }
 
     private static bool IsMatching(Dictionary<string, string> properties, string key, string value)
@@ -109,4 +155,7 @@ public sealed class CloudFormationMessageReceivedNotificationHandler : INotifica
 
         return false;
     }
+
+    [DebuggerDisplay("{StackName} {Project} {Arn} {Status} {Success}")]
+    private readonly record struct Deployment(string StackName, string Project, string Arn, string Status, bool Success);
 }
