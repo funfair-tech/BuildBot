c0c3aa7991c97da0db5026505a2543cebee612d1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-02-17 13:49:13 +0000

    Logging

--- src/BuildBot.Discord/Publishers/DiscordBotMessageNotificationHandler.cs
+++ src/BuildBot.Discord/Publishers/DiscordBotMessageNotificationHandler.cs
@@ -1,21 +1,30 @@
 using System.Threading;
 using System.Threading.Tasks;
 using BuildBot.Discord.Models;
+using BuildBot.Discord.Publishers.LoggingExtensions;
 using Mediator;
+using Microsoft.Extensions.Logging;
 
 namespace BuildBot.Discord.Publishers;
 
 public sealed class DiscordBotMessageNotificationHandler : INotificationHandler<BotMessage>
 {
     private readonly IMessageChannel<BotMessage> _messageChannel;
+    private readonly ILogger<DiscordBotMessageNotificationHandler> _logger;
 
-    public DiscordBotMessageNotificationHandler(IMessageChannel<BotMessage> messageChannel)
+    public DiscordBotMessageNotificationHandler(
+        IMessageChannel<BotMessage> messageChannel,
+        ILogger<DiscordBotMessageNotificationHandler> logger
+    )
     {
         this._messageChannel = messageChannel;
+        this._logger = logger;
     }
 
     public ValueTask Handle(BotMessage notification, CancellationToken cancellationToken)
     {
+        this._logger.QueueingDiscordMessage(notification.Message.Title);
+
         return this._messageChannel.PublishAsync(
             message: notification,
             cancellationToken: cancellationToken
---
+++ src/BuildBot.Discord/Publishers/LoggingExtensions/DiscordBotMessageNotificationHandlerLoggingExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Logging;
+
+namespace BuildBot.Discord.Publishers.LoggingExtensions;
+
+internal static partial class DiscordBotMessageNotificationHandlerLoggingExtensions
+{
+    [LoggerMessage(
+        eventId: 1,
+        level: LogLevel.Information,
+        message: "DISCORD: Queueing message: {title}"
+    )]
+    public static partial void QueueingDiscordMessage(
+        this ILogger<DiscordBotMessageNotificationHandler> logger,
+        string title
+    );
+}
