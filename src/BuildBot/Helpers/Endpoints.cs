a599d51b2d7b01ad6af1d8acbacc025979d62e6a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 00:19:26 +0000

    Cleanup: Endpoints.cs

--- src/BuildBot/Helpers/Endpoints.cs
+++ src/BuildBot/Helpers/Endpoints.cs
@@ -6,8 +6,6 @@ internal static partial class Endpoints
 {
     public static WebApplication ConfigureEndpoints(this WebApplication app)
     {
-        return app.ConfigureTestEndpoints()
-            .ConfigureCloudformationEndpoints()
-            .ConfigureGitHubEndpoints();
+        return app.ConfigureTestEndpoints().ConfigureCloudformationEndpoints().ConfigureGitHubEndpoints();
     }
 }
