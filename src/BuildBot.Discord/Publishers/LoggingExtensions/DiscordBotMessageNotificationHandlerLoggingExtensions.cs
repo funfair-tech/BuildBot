be1f8d40dc3f5141d5a772a2e5b9d4494fb0dc94
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-27 00:33:16 +0000

    Cleanup: DiscordBotMessageNotificationHandlerLoggingExtensions.cs

--- src/BuildBot.Discord/Publishers/LoggingExtensions/DiscordBotMessageNotificationHandlerLoggingExtensions.cs
+++ src/BuildBot.Discord/Publishers/LoggingExtensions/DiscordBotMessageNotificationHandlerLoggingExtensions.cs
@@ -4,11 +4,7 @@ namespace BuildBot.Discord.Publishers.LoggingExtensions;
 
 internal static partial class DiscordBotMessageNotificationHandlerLoggingExtensions
 {
-    [LoggerMessage(
-        eventId: 1,
-        level: LogLevel.Information,
-        message: "DISCORD: Queueing message: {title}"
-    )]
+    [LoggerMessage(eventId: 1, level: LogLevel.Information, message: "DISCORD: Queueing message: {title}")]
     public static partial void QueueingDiscordMessage(
         this ILogger<DiscordBotMessageNotificationHandler> logger,
         string title
