e6ba7a3062e24d44b11be6f7cc6f76db1b45ea45
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-01 14:22:08 +0000

    Cleanup: ServerStartup.cs

--- src/BuildBot/Helpers/ServerStartup.cs
+++ src/BuildBot/Helpers/ServerStartup.cs
@@ -74,19 +74,30 @@ internal static class ServerStartup
 
         SnsNotificationOptions snsConfiguration = LoadSnsNotificationConfig(config);
 
-        builder.Host.UseWindowsService()
-               .UseSystemd();
-        builder.WebHost.UseKestrel(options: options => SetKestrelOptions(options: options, httpPort: httpPort, httpsPort: httpsPort, h2Port: h2Port, configurationFiledPath: configPath))
-               .UseSetting(key: WebHostDefaults.SuppressStatusMessagesKey, value: "True")
-               .ConfigureLogging((_, logger) => ConfigureLogging(logger));
+        builder.Host.UseWindowsService().UseSystemd();
+        builder
+            .WebHost.UseKestrel(options: options =>
+                SetKestrelOptions(
+                    options: options,
+                    httpPort: httpPort,
+                    httpsPort: httpsPort,
+                    h2Port: h2Port,
+                    configurationFiledPath: configPath
+                )
+            )
+            .UseSetting(key: WebHostDefaults.SuppressStatusMessagesKey, value: "True")
+            .ConfigureLogging((_, logger) => ConfigureLogging(logger));
 
-        builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(index: 0, item: AppSerializationContext.Default))
-               .AddDiscord(discordConfig)
-               .AddCloudFormation(snsConfiguration)
-               .AddGitHub()
-               .AddWatchtower()
-               .AddStatus()
-               .AddMediator();
+        builder
+            .Services.ConfigureHttpJsonOptions(options =>
+                options.SerializerOptions.TypeInfoResolverChain.Insert(index: 0, item: AppSerializationContext.Default)
+            )
+            .AddDiscord(discordConfig)
+            .AddCloudFormation(snsConfiguration)
+            .AddGitHub()
+            .AddWatchtower()
+            .AddStatus()
+            .AddMediator();
 
         return builder.Build();
     }
@@ -101,51 +112,60 @@ internal static class ServerStartup
         return new(TopicArn: topicArn, Region: region, AccessKey: accessKeyId, SecretKey: secretKey);
     }
 
-    [SuppressMessage(category: "Microsoft.Reliability", checkId: "CA2000:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
-    [SuppressMessage(category: "SmartAnalyzers.CSharpExtensions.Annotations", checkId: "CSE007:DisposeObjectsBeforeLosingScope", Justification = "Lives for program lifetime")]
+    [SuppressMessage(
+        category: "Microsoft.Reliability",
+        checkId: "CA2000:DisposeObjectsBeforeLosingScope",
+        Justification = "Lives for program lifetime"
+    )]
+    [SuppressMessage(
+        category: "SmartAnalyzers.CSharpExtensions.Annotations",
+        checkId: "CSE007:DisposeObjectsBeforeLosingScope",
+        Justification = "Lives for program lifetime"
+    )]
     private static void ConfigureLogging(ILoggingBuilder logger)
     {
-        logger.ClearProviders()
-              .AddSerilog(CreateLogger(), dispose: true);
+        logger.ClearProviders().AddSerilog(CreateLogger(), dispose: true);
     }
 
     private static Logger CreateLogger()
     {
-        return new LoggerConfiguration().Enrich.WithDemystifiedStackTraces()
-                                        .Enrich.FromLogContext()
-                                        .Enrich.WithMachineName()
-                                        .Enrich.WithProcessId()
-                                        .Enrich.WithThreadId()
-                                        .Enrich.WithProperty(name: "ServerVersion", value: VersionInformation.Version)
-                                        .Enrich.WithProperty(name: "ProcessName", value: VersionInformation.Product)
-                                        .WriteToDebuggerAwareOutput()
-                                        .CreateLogger();
+        return new LoggerConfiguration()
+            .Enrich.WithDemystifiedStackTraces()
+            .Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .Enrich.WithProcessId()
+            .Enrich.WithThreadId()
+            .Enrich.WithProperty(name: "ServerVersion", value: VersionInformation.Version)
+            .Enrich.WithProperty(name: "ProcessName", value: VersionInformation.Product)
+            .WriteToDebuggerAwareOutput()
+            .CreateLogger();
     }
 
     private static LoggerConfiguration WriteToDebuggerAwareOutput(this LoggerConfiguration configuration)
     {
         LoggerSinkConfiguration writeTo = configuration.WriteTo;
 
-        return Debugger.IsAttached
-            ? writeTo.Debug()
-            : writeTo.Console();
+        return Debugger.IsAttached ? writeTo.Debug() : writeTo.Console();
     }
 
     private static IConfigurationRoot LoadConfiguration(string configPath)
     {
-        return new ConfigurationBuilder().SetBasePath(configPath)
-                                         .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
-                                         .AddJsonFile(path: "appsettings-local.json", optional: true, reloadOnChange: false)
-                                         .AddEnvironmentVariables()
-                                         .Build();
+        return new ConfigurationBuilder()
+            .SetBasePath(configPath)
+            .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: false)
+            .AddJsonFile(path: "appsettings-local.json", optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables()
+            .Build();
     }
 
     private static DiscordBotConfiguration LoadDiscordConfig(IConfigurationRoot configuration)
     {
-        return new(server: configuration["Discord:Server"] ?? string.Empty,
-                   channel: configuration["Discord:Channel"] ?? string.Empty,
-                   releaseChannel: configuration["Discord:ReleaseChannel"] ?? string.Empty,
-                   token: configuration["Discord:Token"] ?? string.Empty);
+        return new(
+            server: configuration["Discord:Server"] ?? string.Empty,
+            channel: configuration["Discord:Channel"] ?? string.Empty,
+            releaseChannel: configuration["Discord:ReleaseChannel"] ?? string.Empty,
+            token: configuration["Discord:Token"] ?? string.Empty
+        );
     }
 
     private static void SetH1ListenOptions(ListenOptions listenOptions)
@@ -174,7 +194,13 @@ internal static class ServerStartup
         }
     }
 
-    private static void SetKestrelOptions(KestrelServerOptions options, int httpPort, int httpsPort, int h2Port, string configurationFiledPath)
+    private static void SetKestrelOptions(
+        KestrelServerOptions options,
+        int httpPort,
+        int httpsPort,
+        int h2Port,
+        string configurationFiledPath
+    )
     {
         options.DisableStringReuse = false;
         options.AllowSynchronousIO = false;
@@ -186,7 +212,11 @@ internal static class ServerStartup
         if (httpsPort != 0)
         {
             Console.WriteLine($"Listening on HTTPS port: {httpsPort}");
-            options.Listen(address: IPAddress.Any, port: httpsPort, configure: o => SetHttpsListenOptions(listenOptions: o, configurationFiledPath: configurationFiledPath));
+            options.Listen(
+                address: IPAddress.Any,
+                port: httpsPort,
+                configure: o => SetHttpsListenOptions(listenOptions: o, configurationFiledPath: configurationFiledPath)
+            );
         }
 
         if (h2Port != 0)
