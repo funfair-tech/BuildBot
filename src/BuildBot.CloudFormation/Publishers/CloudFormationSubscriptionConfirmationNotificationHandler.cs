b2c75e95e3ce21e25d5d286062dd9b459dc1995f
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-08-14 23:55:06 +0000

    Cleanup: CloudFormationSubscriptionConfirmationNotificationHandler.cs

--- src/BuildBot.CloudFormation/Publishers/CloudFormationSubscriptionConfirmationNotificationHandler.cs
+++ src/BuildBot.CloudFormation/Publishers/CloudFormationSubscriptionConfirmationNotificationHandler.cs
@@ -11,24 +11,38 @@ using Microsoft.Extensions.Logging;
 
 namespace BuildBot.CloudFormation.Publishers;
 
-public sealed class CloudFormationSubscriptionConfirmationNotificationHandler : INotificationHandler<CloudFormationSubscriptionConfirmation>
+public sealed class CloudFormationSubscriptionConfirmationNotificationHandler
+    : INotificationHandler<CloudFormationSubscriptionConfirmation>
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<CloudFormationSubscriptionConfirmationNotificationHandler> _logger;
     private readonly SnsNotificationOptions _options;
 
-    [SuppressMessage(category: "Roslynator.Analyzers", checkId: "RCS1231: Make parameter ref read-only.", Justification = "DI")]
-    public CloudFormationSubscriptionConfirmationNotificationHandler(IHttpClientFactory httpClientFactory,
-                                                                     SnsNotificationOptions options,
-                                                                     ILogger<CloudFormationSubscriptionConfirmationNotificationHandler> logger)
+    [SuppressMessage(
+        category: "Roslynator.Analyzers",
+        checkId: "RCS1231: Make parameter ref read-only.",
+        Justification = "DI"
+    )]
+    public CloudFormationSubscriptionConfirmationNotificationHandler(
+        IHttpClientFactory httpClientFactory,
+        SnsNotificationOptions options,
+        ILogger<CloudFormationSubscriptionConfirmationNotificationHandler> logger
+    )
     {
         this._httpClientFactory = httpClientFactory;
         this._options = options;
         this._logger = logger;
     }
 
-    [SuppressMessage(category: "", checkId: "CSE007: Handle dispose correctly.", Justification = "HttpClient is managed by the HttpClientFactory.")]
-    public async ValueTask Handle(CloudFormationSubscriptionConfirmation notification, CancellationToken cancellationToken)
+    [SuppressMessage(
+        category: "",
+        checkId: "CSE007: Handle dispose correctly.",
+        Justification = "HttpClient is managed by the HttpClientFactory."
+    )]
+    public async ValueTask Handle(
+        CloudFormationSubscriptionConfirmation notification,
+        CancellationToken cancellationToken
+    )
     {
         if (!this._options.IsValidArn(notification.TopicArn))
         {
@@ -39,14 +53,23 @@ public sealed class CloudFormationSubscriptionConfirmationNotificationHandler : 
 
         try
         {
-            HttpClient client = this._httpClientFactory.CreateClient(nameof(CloudFormationSubscriptionConfirmationNotificationHandler));
+            HttpClient client = this._httpClientFactory.CreateClient(
+                nameof(CloudFormationSubscriptionConfirmationNotificationHandler)
+            );
 
-            HttpResponseMessage responseMessage = await client.GetAsync(requestUri: notification.SubscribeUrl, cancellationToken: cancellationToken);
+            HttpResponseMessage responseMessage = await client.GetAsync(
+                requestUri: notification.SubscribeUrl,
+                cancellationToken: cancellationToken
+            );
             responseMessage.EnsureSuccessStatusCode();
         }
         catch (Exception exception)
         {
-            this._logger.FailedToSubscribeToTopic(subscribeUrl: notification.SubscribeUrl, message: exception.Message, exception: exception);
+            this._logger.FailedToSubscribeToTopic(
+                subscribeUrl: notification.SubscribeUrl,
+                message: exception.Message,
+                exception: exception
+            );
         }
     }
 }
